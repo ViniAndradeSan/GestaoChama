151ddb872c8caeda2e87699c2c92cc06a1c7135b
Vinicius <viniciusandrade0521@gmail.com>
2026-06-08 09:35:41 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoChama/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestaoChama
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Contato { get; set; }
+
+        public Cliente(string nome, string contato)
+        {
+            Nome = nome;
+            Contato = contato;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} ({Contato})";
+        }
+    }
+}
---
+++ GestaoChama/Form1.cs
@@ -0,0 +1,10 @@
+namespace GestaoChama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
